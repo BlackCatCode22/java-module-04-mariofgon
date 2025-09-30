6fc4c9e818e0b239e3c6e67971bbefccee90df5c
mariofgon <0769210@my.scccd.edu>
2025-09-30 10:54:50 -0700

    Add files via upload

---
+++ Maps.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class Maps {
+    public static void main(String[] args){
+
+        HashMap<String, Integer> empIDs = new HashMap<>();
+
+        empIDs.put("John", 12345);
+        empIDs.put("Carl", 54321);
+        empIDs.put("Jerry", 8675309);
+
+        System.out.println(empIDs);
+        System.out.println(empIDs.get("Carl"));
+        System.out.println(empIDs.containsKey("Jerry"));
+        System.out.println(empIDs.containsValue(8675309));
+
+        empIDs.put("John", 98765);
+        System.out.println(empIDs);
+
+        empIDs.replace("Kramer", 777);
+        System.out.println(empIDs);
+
+        empIDs.putIfAbsent("Steve", 222);
+        System.out.println(empIDs);
+
+        empIDs.remove("Steve");
+        System.out.println(empIDs);
+    }
+}
